7186548262f257e71e4adfce0e6bb203a48d34ed
vanisubodh <vanisubodh@gmail.com>
2021-03-19 15:16:19 +0000

    scraping Indeed

--- JobsWebScrapingIndeed.py
+++ JobsWebScrapingIndeed.py
@@ -1,1 +1,10 @@
-Hi
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.indeed.com/jobs?q=Entry+Level+ASP+Net&l=Washington%2C+DC'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.findAll('h2')
+
+print(results)
